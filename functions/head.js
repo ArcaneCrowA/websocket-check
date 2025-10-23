8fceca573987c996069cc243639d1366facda18f
ArcaneCrowA <adiletbeishekeyev@gmail.com>
2025-10-23 16:20:19 +0500

    f

---
+++ functions/head.js
@@ -0,0 +1,5 @@
+window.n9s =
+  window.n9s ||
+  function () {
+    (n9s.q = n9s.q || []).push(arguments);
+  };
---
+++ functions/optional/end_session.js
@@ -0,0 +1,3 @@
+n9s("shutdown");
+// if needed, you can call init again for a new user
+// n9s('init', {...})
