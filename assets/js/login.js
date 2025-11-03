622a99a1b6c36fa2ae583d9a20de88858361b50d
Adilet <adiletbeishekeyev@gmail.com>
2025-11-03 10:50:49 +0500

    removed netlify, changed to actual server

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    fetch("https://example.com/api/login", {
+    fetch("http://23.88.42.12/api/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
